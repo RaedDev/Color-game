7a13f6749547ce37fc9c0c850c74c901f74c7b6c
Raed Ibrahim Abdullah <Raed.Dev@gmail.com>
2017-11-27 21:24:30 +0200

    Adding npm packages

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', 80);
+console.log("Running...");
+app.use(express.static(path.join(__dirname, 'public')));
 
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
-    Console.log('Magic happens on port ' + port);
+    console.log('Magic happens on port ' + port);
 });
